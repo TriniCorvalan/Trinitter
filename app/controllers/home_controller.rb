455f40fc12a78351dae62b085567fa1891cc3d28
Trini <trini.4595@gmail.com>
2020-12-14 20:38:41 -0300

    add profile

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,6 @@ class HomeController < ApplicationController
   end
 
   def tweets
-    @tweets = current_user.tweets
+    @tweets = current_user.tweets.order('created_at DESC').page(params[:page]).per(50)
   end
 end
